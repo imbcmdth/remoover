c5bf2e5f3d6ed716685a97f5ea7fad9ebf13e719
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2019-09-02 23:49:35 -0500

    Added a license and clarified the license of the contrib files

--- contrib/mp4-generator.js
+++ contrib/mp4-generator.js
@@ -1,8 +1,21 @@
+/******************************************************************************
+  Copyright Brightcove, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+******************************************************************************/
+
 /**
  * mux.js
- *
- * Copyright (c) 2015 Brightcove
- * All rights reserved.
  *
  * Functions that generate fragmented MP4s suitable for use with Media
  * Source Extensions.
